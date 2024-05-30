11318873c51add109d3cf56ddbb08cca6fbd18e4
Hemanth k <hemanthss24@gmail.com>
2024-05-30 14:37:54 +0530

    fisrt commit

---
+++ script.js
@@ -0,0 +1,9 @@
+window.alert("hello there!");
+
+setTimeout(()=>{
+    window.alert("hello there")
+}, 7000);
+
+function helloWorld = ( () => {console.log('hello world')});
+
+setInterval( () => {console.log('hey'), 5000});
